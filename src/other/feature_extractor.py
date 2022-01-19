5d71144a3b30af4577723d27070b1e2e84cb02e6
Alireza Rahimi <rahimialireza.az@gmail.com>
2022-01-19 18:56:22 +0330

    add parallel mode for creating features

--- src/other/feature_extractor.py
+++ src/other/feature_extractor.py
@@ -1,9 +1,13 @@
+import multiprocessing
+import os
+import random
+
 import cv2 as cv
 import numpy as np
 import pandas as pd
-import os
+from joblib import Parallel, delayed
+
 import util
-import random
 
 # params for ShimTomasi corner detection
 feature_params = dict(maxCorners=100,
@@ -34,35 +38,35 @@ def count_frames(video_path):
 
 def gen_sift_features(frame):
     sift = cv.SIFT_create()
-    keypoints, descriptors = sift.detectAndCompute(frame, None)
+    key_points, descriptors = sift.detectAndCompute(frame, None)
 
-    return keypoints, descriptors
-
-
-def keypoints_to_coordinates(keypoints):
-    keypoints_xy = []
-
-    for kp in keypoints:
-        keypoints_xy.append([kp.pt[0], kp.pt[1]])
-
-    return keypoints_xy
+    return key_points, descriptors
 
 
-def has_sufficient_motion(keypoints_xy, keypoints, descriptors, p1, lambd):
-    sm_keypoints_xy = []
-    sm_keypoints = []
+def key_points_to_coordinates(key_points):
+    key_points_xy = []
+
+    for kp in key_points:
+        key_points_xy.append([kp.pt[0], kp.pt[1]])
+
+    return key_points_xy
+
+
+def has_sufficient_motion(key_points_xy, key_points, descriptors, p1, lambda_value):
+    sm_key_points_xy = []
+    sm_key_points = []
     sm_descriptors = []
 
-    for i in range(len(keypoints)):
-        distance_x = keypoints_xy[i].T[0] - p1[i].T[0]
-        distance_y = keypoints_xy[i].T[1] - p1[i].T[1]
+    for i in range(len(key_points)):
+        distance_x = key_points_xy[i].T[0] - p1[i].T[0]
+        distance_y = key_points_xy[i].T[1] - p1[i].T[1]
 
-        if distance_x > lambd or distance_y > lambd:
-            sm_keypoints_xy.append([int(keypoints_xy[i].T[0]), int(keypoints_xy[i].T[1])])
-            sm_keypoints.append(keypoints[i])
+        if distance_x > lambda_value or distance_y > lambda_value:
+            sm_key_points_xy.append([int(key_points_xy[i].T[0]), int(key_points_xy[i].T[1])])
+            sm_key_points.append(key_points[i])
             sm_descriptors.append(descriptors[i])
 
-    return sm_keypoints_xy, sm_keypoints, sm_descriptors
+    return sm_key_points_xy, sm_key_points, sm_descriptors
 
 
 def gen_hof(x, y, frame, next_frame):
@@ -82,24 +86,24 @@ def gen_hof(x, y, frame, next_frame):
     return hof
 
 
-def gen_mosift_features(video_path, lambd, interval, sample_size):
+def gen_mosift_features(video_path, lambda_value, interval, sample_size):
     frames = count_frames(video_path)
     mosift_descriptors = []
 
     for i in range(1, frames - 1, interval):
         ret, frame = capture_frame(video_path, i)
         ret, next_frame = capture_frame(video_path, i + 1)
-        keypoints, descriptors = gen_sift_features(frame)
-        if len(keypoints) == 0:
+        key_points, descriptors = gen_sift_features(frame)
+        if len(key_points) == 0:
             continue
-        keypoints_xy = keypoints_to_coordinates(keypoints)
-        keypoints_xy = np.float32(np.array(keypoints_xy)[:, np.newaxis, :])
-        p1, st, err = cv.calcOpticalFlowPyrLK(frame, next_frame, keypoints_xy, None, **lk_params)
-        sm_keypoints_xy, sm_keypoints, sm_descriptors = has_sufficient_motion(keypoints_xy, keypoints, descriptors,
-                                                                              p1, lambd)
+        key_points_xy = key_points_to_coordinates(key_points)
+        key_points_xy = np.float32(np.array(key_points_xy)[:, np.newaxis, :])
+        p1, st, err = cv.calcOpticalFlowPyrLK(frame, next_frame, key_points_xy, None, **lk_params)
+        sm_key_points_xy, sm_key_points, sm_descriptors = has_sufficient_motion(key_points_xy, key_points, descriptors,
+                                                                                p1, lambda_value)
 
-        for j in range(len(sm_keypoints)):
-            hof = np.array(gen_hof(sm_keypoints_xy[j][0], sm_keypoints_xy[j][1], frame, next_frame))
+        for j in range(len(sm_key_points)):
+            hof = np.array(gen_hof(sm_key_points_xy[j][0], sm_key_points_xy[j][1], frame, next_frame))
             mosift_descriptors.append(list(np.concatenate((sm_descriptors[j], hof))))
 
     random_mosift_descriptors = random.sample(mosift_descriptors, k=int(len(mosift_descriptors) * sample_size))
@@ -107,7 +111,7 @@ def gen_mosift_features(video_path, lambd, interval, sample_size):
     return random_mosift_descriptors
 
 
-def run_feature_extractor(input_path, output_path, lambd, interval, dict_directory, sample_size):
+def run_feature_extractor(input_path, output_path, lambda_value, interval, dict_directory, sample_size):
     listing = os.listdir(input_path)
     progress_count = 0
     for video_name in listing:
@@ -116,17 +120,26 @@ def run_feature_extractor(input_path, output_path, lambd, interval, dict_directo
         video_path = input_path + video_name
 
         if dict_directory:
-            df_dict = pd.DataFrame(gen_mosift_features(video_path, lambd, interval, sample_size))
+            df_dict = pd.DataFrame(gen_mosift_features(video_path, lambda_value, interval, sample_size))
             df_dict.to_csv(output_path + "dict.csv", mode='a', header=False, index=False)
         else:
-            df_mosift_features = pd.DataFrame(gen_mosift_features(video_path, lambd, interval, sample_size))
+            df_mosift_features = pd.DataFrame(gen_mosift_features(video_path, lambda_value, interval, sample_size))
             df_mosift_features.to_csv(output_path + video_name[:-4] + ".csv", mode='a', header=False, index=False)
 
 
 if __name__ == '__main__':
-    run_feature_extractor(r"/home/alireza/projects/python/MoSIFT/dataset/KTH/running/",
-                          r"/home/alireza/projects/python/MoSIFT/dataset/csv/", 0.7, 1, True, 0.2)
-    # run_feature_extractor(r"/home/arnaldo/Documentos/rwf-indoor-dataset-separada/validation/assault/",
-    #                       r"/home/arnaldo/Documentos/rwf-indoor-dataset-separada/csv/assault/", 0.7, 1, False, 1)
-    # run_feature_extractor(r"/home/arnaldo/Documentos/rwf-indoor-dataset-separada/validation/non-assault/",
-    #                       r"/home/arnaldo/Documentos/rwf-indoor-dataset-separada/csv/non-assault/", 0.7, 1, False, 1)
+    paths = [(r"/home/alireza/projects/python/MoSIFT/dataset/KTH/running/",
+              r"/home/alireza/projects/python/MoSIFT/dataset/csv/running/"),
+             (r"/home/alireza/projects/python/MoSIFT/dataset/KTH/boxing/",
+              r"/home/alireza/projects/python/MoSIFT/dataset/csv/boxing/"),
+             (r"/home/alireza/projects/python/MoSIFT/dataset/KTH/handclapping/",
+              r"/home/alireza/projects/python/MoSIFT/dataset/csv/handclapping/"),
+             (r"/home/alireza/projects/python/MoSIFT/dataset/KTH/handwaving/",
+              r"/home/alireza/projects/python/MoSIFT/dataset/csv/handwaving/"),
+             (r"/home/alireza/projects/python/MoSIFT/dataset/KTH/jogging/",
+              r"/home/alireza/projects/python/MoSIFT/dataset/csv/jogging/"),
+             (r"/home/alireza/projects/python/MoSIFT/dataset/KTH/walking/",
+              r"/home/alireza/projects/python/MoSIFT/dataset/csv/walking/")]
+
+    Parallel(n_jobs=int(multiprocessing.cpu_count()))(
+        delayed(run_feature_extractor)(path, target, 0.7, 1, False, 0.2) for path, target in paths)
